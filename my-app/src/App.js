48663413ac4b80cd9639dfbb0fa2db657b48e5d7
Alex Ngan <a.ngan@hotmail.co.uk>
2022-11-03 18:16:16 +0000

    second part

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import Header from './components/Header';
-import About from './pages/About';
+import {useEffect,useState} from 'react'
+import Header from './components/Header'
+import About from './pages/About'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
+import axios from 'axios';
+
 
 function App() {
+
+const [characters,setCharacters]=useState([])
+
+
+useEffect(()=>{
+ axios.get(`https://rickandmortyapi.com/api/character`)
+ .then(res=>{
+  console.log(res.data.results)
+  setCharacters(res.data.results)
+ })
+ .catch(err=>console.log(err))
+
+
+},[])
 
 
 
@@ -14,10 +31,11 @@ function App() {
     <BrowserRouter>
          <Header/>
          <Routes>
-             <Route path="/" element={<HomePage/>}/>
+             <Route path="/" element={<HomePage characters={characters}/>}/>
              <Route path="/about" element={<About/>}/>
          </Routes>
     </BrowserRouter>
   ); 
 }
+
 export default App;
--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Search from '../components/Search'
+import CharacterCard from '../components/CharacterCard'
+import '../styles/home.css';
 
-function HomePage() {
+function HomePage({characters}) {
   return (
-    <div>HomePage</div>
+    <div>
+      <Search/>
+      <h1>Main Characters</h1>
+      <div className='characters-container'>
+        {
+          characters.map((item)=>{
+            return <CharacterCard key={item.id} character={item}/>
+            
+            
+
+          })
+        }
+      </div>
+    </div>
   )
 }
 
